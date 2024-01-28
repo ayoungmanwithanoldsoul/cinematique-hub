703df92da157848611f44f439c9bd822100bf05f
Andrew <drewstackdev@gmail.com>
2024-01-28 22:56:38 +0800

    Initialize repo

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module edu.umindanao.cinematiquehub {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens edu.umindanao.cinematiquehub to javafx.fxml;
+    exports edu.umindanao.cinematiquehub;
+}
